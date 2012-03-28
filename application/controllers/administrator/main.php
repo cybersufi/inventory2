413f29ab9783f9e6b2c55388248ac8acb0714279
Bayu Krisnamurti <cybersufi@gmail.com>
2012-03-28 18:23:33 +0700

    qw

--- application/controllers/administrator/main.php
+++ application/controllers/administrator/main.php
@@ -3,18 +3,21 @@
 class Main extends CI_Controller {
 		
 	private $base_url;
+	private $sitename;
 	
 	public function __construct() {
 		parent::__construct();
 		$this->CI =& get_Instance();
 		$this->base_url = $this->CI->config->item('base_url');
+		$this->sitename = $this->CI->config->item('site_name');
 	}
 	
 	public function index()
 	{
 		//$data['base_url'] = $this->base_url;
 		$data['base_url'] = base_url();
-		$this->load->view('administrator/login2/login_index', $data);
+		$data['site_name'] = $this->sitename;
+		$this->load->view('administrator/main2/main_index', $data);
 	}
 }
 
---
+++ application/views/administrator/main2/main_index.php
@@ -0,0 +1,207 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<html lang="en">
+<head>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php echo $site_name; ?> - Dashboard Admin Panel</title>
+	<?php
+		$this->asset->stylesheet('administrator/main/layout');
+		$this->asset->javascript('administrator/main/jquery-1.5.2.min');
+		$this->asset->javascript('administrator/main/hideshow');
+		$this->asset->javascript('administrator/main/jquery.tablesorter.min');
+		$this->asset->javascript('administrator/main/jquery.equalHeight');
+	?>
+	<!--[if lt IE 9]>
+	<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script type="text/javascript">
+		$(document).ready(function() { 
+			$(".tablesorter").tablesorter(); 
+	   	});
+		$(document).ready(function() {
+			//When page loads...
+			$(".tab_content").hide(); //Hide all content
+			$("ul.tabs li:first").addClass("active").show(); //Activate first tab
+			$(".tab_content:first").show(); //Show first tab content
+
+			//On Click Event
+			$("ul.tabs li").click(function() {
+
+				$("ul.tabs li").removeClass("active"); //Remove any "active" class
+				$(this).addClass("active"); //Add "active" class to selected tab
+				$(".tab_content").hide(); //Hide all tab content
+
+				var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+				$(activeTab).fadeIn(); //Fade in the active ID content
+				return false;
+			});
+
+		});
+    </script>
+    <script type="text/javascript">
+	    $(function(){
+	        $('.column').equalHeight();
+	    });
+	</script>
+</head>
+<body>
+	<header id="header">
+		<hgroup>
+			<h1 class="site_title"><a href="index.html">Site Administration</a></h1>
+			<h2 class="section_title">Dashboard</h2><div class="btn_view_site"><a href="http://www.medialoot.com">View Site</a></div>
+		</hgroup>
+	</header> <!-- end of header bar -->
+	
+	<section id="secondary_bar">
+		<div class="user">
+			<p>John Doe (<a href="#">3 Messages</a>)</p>
+			<!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
+		</div>
+		<div class="breadcrumbs_container">
+			<article class="breadcrumbs"><a href="index.html">Website Admin</a> <div class="breadcrumb_divider"></div> <a class="current">Dashboard</a></article>
+		</div>
+	</section><!-- end of secondary bar -->
+	
+	<aside id="sidebar" class="column">
+		<hr/>
+		<h3>Content</h3>
+		<ul class="toggle">
+			<li class="icn_new_article"><a href="#">New Link</a></li>
+			<li class="icn_edit_article"><a href="#">Edit Link</a></li>
+			<li class="icn_categories"><a href="#">Show Links</a></li>
+		</ul>
+		<h3>Users</h3>
+		<ul class="toggle">
+			<li class="icn_add_user"><a href="#">Add New User</a></li>
+			<li class="icn_view_users"><a href="#">View Users</a></li>
+			<li class="icn_profile"><a href="#">Your Profile</a></li>
+		</ul>
+		<h3>Groups</h3>
+		<ul class="toggle">
+			<li class="icn_add_user"><a href="#">Add New Group</a></li>
+			<li class="icn_view_users"><a href="#">View Groups</a></li>
+		</ul>
+		<h3>Media</h3>
+		<ul class="toggle">
+			<li class="icn_folder"><a href="#">File Manager</a></li>
+			<li class="icn_photo"><a href="#">Gallery</a></li>
+			<li class="icn_audio"><a href="#">Audio</a></li>
+			<li class="icn_video"><a href="#">Video</a></li>
+		</ul>
+		<h3>Admin</h3>
+		<ul class="toggle">
+			<li class="icn_settings"><a href="#">Options</a></li>
+			<li class="icn_security"><a href="#">Security</a></li>
+			<li class="icn_jump_back"><a href="#">Logout</a></li>
+		</ul>
+		
+		<footer>
+			<hr />
+			<p><strong>Copyright &copy; 2011 UNIX Team</strong></p>
+			<p>Theme by <a href="http://www.medialoot.com">MediaLoot</a></p>
+		</footer>
+	</aside><!-- end of sidebar -->
+	
+	<section id="main" class="column">
+		
+		<h4 class="alert_info">Welcome to the free MediaLoot admin panel template, this could be an informative message.</h4>
+		
+		<article class="module width_full">
+			<header><h3>Stats</h3></header>
+			<div class="module_content">
+				<article class="stats_graph">
+					<img src="http://chart.apis.google.com/chart?chxr=0,0,3000&chxt=y&chs=520x140&cht=lc&chco=76A4FB,80C65A&chd=s:Tdjpsvyvttmiihgmnrst,OTbdcfhhggcTUTTUadfk&chls=2|2&chma=40,20,20,30" width="520" height="140" alt="" />
+				</article>
+				
+				<article class="stats_overview">
+					<div class="overview_today">
+						<p class="overview_day">Today</p>
+						<p class="overview_count">1,876</p>
+						<p class="overview_type">Hits</p>
+						<p class="overview_count">2,103</p>
+						<p class="overview_type">Views</p>
+					</div>
+					<div class="overview_previous">
+						<p class="overview_day">Yesterday</p>
+						<p class="overview_count">1,646</p>
+						<p class="overview_type">Hits</p>
+						<p class="overview_count">2,054</p>
+						<p class="overview_type">Views</p>
+					</div>
+				</article>
+				<div class="clear"></div>
+			</div>
+		</article><!-- end of stats article -->
+		
+		<div class="clear"></div>
+		
+		<article class="module width_full">
+			<header><h3>Post New Article</h3></header>
+				<div class="module_content">
+						<fieldset>
+							<label>Post Title</label>
+							<input type="text">
+						</fieldset>
+						<fieldset>
+							<label>Content</label>
+							<textarea rows="12"></textarea>
+						</fieldset>
+						<fieldset style="width:48%; float:left; margin-right: 3%;"> <!-- to make two field float next to one another, adjust values accordingly -->
+							<label>Category</label>
+							<select style="width:92%;">
+								<option>Articles</option>
+								<option>Tutorials</option>
+								<option>Freebies</option>
+							</select>
+						</fieldset>
+						<fieldset style="width:48%; float:left;"> <!-- to make two field float next to one another, adjust values accordingly -->
+							<label>Tags</label>
+							<input type="text" style="width:92%;">
+						</fieldset><div class="clear"></div>
+				</div>
+			<footer>
+				<div class="submit_link">
+					<select>
+						<option>Draft</option>
+						<option>Published</option>
+					</select>
+					<input type="submit" value="Publish" class="alt_btn">
+					<input type="submit" value="Reset">
+				</div>
+			</footer>
+		</article><!-- end of post new article -->
+		
+		<h4 class="alert_warning">A Warning Alert</h4>
+		
+		<h4 class="alert_error">An Error Message</h4>
+		
+		<h4 class="alert_success">A Success Message</h4>
+		
+		<article class="module width_full">
+			<header><h3>Basic Styles</h3></header>
+				<div class="module_content">
+					<h1>Header 1</h1>
+					<h2>Header 2</h2>
+					<h3>Header 3</h3>
+					<h4>Header 4</h4>
+					<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum. Maecenas faucibus mollis interdum. Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+
+<p>Donec id elit non mi porta <a href="#">link text</a> gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+
+					<ul>
+						<li>Donec ullamcorper nulla non metus auctor fringilla. </li>
+						<li>Cras mattis consectetur purus sit amet fermentum.</li>
+						<li>Donec ullamcorper nulla non metus auctor fringilla. </li>
+						<li>Cras mattis consectetur purus sit amet fermentum.</li>
+					</ul>
+				</div>
+		</article><!-- end of styles article -->
+		<div class="spacer"></div>
+	</section>
+
+
+</body>
+</html>
