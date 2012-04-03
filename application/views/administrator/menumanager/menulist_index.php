5086b838da91bb208619ac8076bcf1567dbd9831
unknown <Lenovo@DEA2-NOC-FO-C06.(none)>
2012-04-04 01:31:53 +0700

    as

--- application/views/administrator/menumanager/menulist_index.php
+++ application/views/administrator/menumanager/menulist_index.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <article class="module width_full">
-<header><h3>Menu List</h3></header>
+<header><h3 class="tabs_involved">Menu List</h3></header>
 	<div class="module_content" style="height: 500px">
 		<div id="menu_grid"></div>
 	</div>
